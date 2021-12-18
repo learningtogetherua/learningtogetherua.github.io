538ca93d6a55241b0e0d051cd761162a13e2a047
learningtogetherua <learningtogetherua@gmail.com>
2021-12-19 01:10:56 +0200

    qq
    qqq

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $('.nav__btn').on('click', function(){
         $('.nav__menu').toggleClass('nav__menu-active');
     })
@@ -8,9 +7,6 @@ $(function(){
         {
             arrows: false,
             dots: true,
-            // autoplay: true,
-            // fade: true,
-           
         }
     )
 
@@ -18,59 +14,14 @@ $(function(){
         {
             arrows: false,
             dots: true,
-            // autoplay: true,
-            // fade: true,
-           
         }
     )
 
-    $('.teachers__slider').slick(
-        {
-            arrows: false,
-            dots: true,
-            // autoplay: true,
-            // fade: true,
-           
-        }
-    )
     $('.client__slider').slick(
         {
             arrows: false,
             dots: true,
-            // autoplay: true,
-            // fade: true,
-           
         }
     )
 
-    // $('.reviews__slider').slick({
-    //     arrows: false,
-    //     dots: true,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 2,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1140,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //                 slidesToScroll: 3
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 840,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 2
-
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 580,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
-    //     ]
-    // })
 });
